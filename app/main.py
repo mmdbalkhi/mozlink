a0ce5d4f781e3875ea3d768e38a3310c5467e7c8
komeil Parseh <ahmdparsh129@gmail.com>
2022-06-02 20:18:27 +0430

    support SQlAlchemy

--- app/main.py
+++ app/main.py
@@ -1,21 +1,33 @@
 #!env/bin/python3
 """this app power by flask
 with this web app make short link!"""
-
-from config import DB, HOST, PASSWORD, SQL, USERNAME
-from flask import Flask, flash, redirect, render_template, request
-from modules import SECRET_KEY, MySql, SqlLitedb, hashids, is_valid
+import config
+from flask import flash
+from flask import Flask
+from flask import redirect
+from flask import render_template
+from flask import request
+from models import db
+from models import hashids
+from models import Urls
 
 app = Flask(__name__)
+app.secret_key = config.SECRET_KEY
 
-sql = (
-    SqlLitedb(path="database.db")
-    if SQL == "sqlite"
-    else MySql(host=HOST, username=USERNAME, password=PASSWORD, db=DB)
-)
+app.config["SQLALCHEMY_DATABASE_URI"] = config.DATABASE_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = config.TRACK_MODIFICATIONS
 
-sql.create_link_table()
-app.secret_key = SECRET_KEY
+with app.app_context():
+    db.init_app(app)
+    db.create_all()
+
+url = Urls()
+
+
+@app.route("/about")
+def about_page():
+    """about page"""
+    return render_template("about.html")
 
 
 @app.route("/", methods=("GET", "POST"))
@@ -27,14 +39,11 @@ def index():
         url = request.form["url"]
 
         # If user Enter empty Value; Flashing(!) Of the "The URL is required!"
-        if not url or not is_valid(url):
+        if not url:
             flash("The URL is required!")
             return render_template("index.html"), 405
 
-        if "http" not in url:
-            url = "http://" + url
-
-        url_id = sql.write(url)
+        url_id = Urls(original_url=url).save()
         short_url = request.host_url + hashids.encode(url_id)
 
         return render_template("index.html", short_url=short_url)
@@ -45,16 +54,14 @@ def index():
 @app.route("/<url_id>")
 def url_redirect(url_id):
     """redirected "mozLink!" URL to orginal Url"""
-
-    original_url = sql.read(url_id)
-
+    original_url = url.get(id=url_id)
+    print(original_url)
     if original_url:
-        return redirect(original_url)
+        return redirect(original_url.original_url)
 
     flash("Invalid URL")
     return render_template("index.html"), 404
 
 
 if __name__ == "__main__":
-    sql.create_link_table()
     app.run("0.0.0.0", 5000, debug=False)
